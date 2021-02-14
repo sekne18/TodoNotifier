f544c9e1b3faed55c561dcf507ccf43c3dfabe46
sekne18 <sekne50@gmail.com>
2021-02-14 22:04:34 +0100

    Added routing and navbar

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalendarComponent } from './calendar/calendar.component';
+import { HomeComponent } from './home/home.component'
+import { NotesComponent } from './notes/notes.component';
+import { TodoComponent } from './todo/todo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'todo', component: TodoComponent},
+  {path: 'calendar', component: CalendarComponent},
+  {path: 'notes', component: NotesComponent},
+  {path: '**', component: HomeComponent, pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,17 +1,36 @@
 import { NgModule } from '@angular/core';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import dayGridPlugin from '@fullcalendar/daygrid'; 
+import interactionPlugin from '@fullcalendar/interaction'; 
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { TodoComponent } from './todo/todo.component';
+import { NotesComponent } from './notes/notes.component';
+import { HomeComponent } from './home/home.component';
+
+FullCalendarModule.registerPlugins([ // register FullCalendar plugins
+  dayGridPlugin,
+  interactionPlugin
+]);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    CalendarComponent,
+    TodoComponent,
+    NotesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FullCalendarModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
